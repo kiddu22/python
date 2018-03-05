17bb8642811dde4789a49db01d4012a39b4ebd1b
kiddu22 <singhneha123397@gmail.com>
2018-03-05 17:52:49 +0530

    my first commit

---
+++ mp.py
@@ -0,0 +1,21 @@
+a=12.5
+b=6
+a+b
+print a+b
+a=a+1
+print a
+print b
+c="welcome"
+print c
+c=c+"welcome"
+print c
+clear
+if a==12.5
+print a
+else print b
+if a==12.5 or c="welcome":
+a=a+1
+c=c+"rcpit"
+print a
+print c
+
